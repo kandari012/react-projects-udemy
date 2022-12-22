955f2f36b59357ee8e22d08c404de80eb3f5d945
rahul kandari <rahulkandari012@gmail.com>
2022-12-22 15:29:08 +0530

    review project

--- 03-reviews/setup/src/App.js
+++ 03-reviews/setup/src/App.js
@@ -1,7 +1,12 @@
-import React from 'react';
-import Review from './Review';
+import React from "react";
+import Review from "./Review";
 function App() {
-  return <h2>reviews project setup</h2>;
+  return (
+    <h2>
+      Our reviews
+      <Review></Review>
+    </h2>
+  );
 }
 
 export default App;
--- 03-reviews/setup/src/Review.js
+++ 03-reviews/setup/src/Review.js
@@ -1,9 +1,41 @@
-import React, { useState } from 'react';
-import people from './data';
-import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
+import React, { useState } from "react";
+import people from "./data";
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
 
 const Review = () => {
-  return <h2>review component</h2>;
+  const [index, setIndex] = useState(0);
+  const { id, name, job, image, text } = people[index];
+  const moveRight = () => {
+    if (index === people.length - 1) {
+      setIndex(0);
+    } else {
+      setIndex(index + 1);
+    }
+  };
+
+  const moveLeft = () => {
+    if (index === 0) {
+      setIndex(people.length - 1);
+    } else {
+      setIndex(index - 1);
+    }
+  };
+
+  const random = () => {
+    const number = Math.floor(Math.random() * people.length);
+    setIndex(number);
+    console.log(number);
+  };
+  return (
+    <main>
+      <img src={image} className="person-img"></img>
+      <h4>{name}</h4>
+      <h4>{job}</h4>
+      <FaChevronLeft onClick={moveLeft} />
+      <FaChevronRight onClick={moveRight} />
+      <button onClick={random}>suprise me</button>
+    </main>
+  );
 };
 
 export default Review;
