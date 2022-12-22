6ec18e358af2920994a1865514abba4f6f6ec7e0
rahul kandari <rahulkandari012@gmail.com>
2022-12-22 17:06:58 +0530

    accrdion project

--- 04-accordion/setup/src/App.js
+++ 04-accordion/setup/src/App.js
@@ -1,8 +1,17 @@
-import React, { useState } from 'react';
-import data from './data';
-import SingleQuestion from './Question';
+import React, { useState } from "react";
+import data from "./data";
+import SingleQuestion from "./Question";
 function App() {
-  return <h2>accordion project setup</h2>;
+  return (
+    <main>
+      <h3>Questions And Answers About Login</h3>
+      <div>
+        {data.map((item) => {
+          return <SingleQuestion item={item} key={item.id} />;
+        })}
+      </div>
+    </main>
+  );
 }
 
 export default App;
--- 04-accordion/setup/src/Question.js
+++ 04-accordion/setup/src/Question.js
@@ -1,7 +1,23 @@
-import React, { useState } from 'react';
-import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+import React, { useState } from "react";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
+const Question = ({ item }) => {
+  const [show, setShow] = useState(false);
+  const flip = () => {
+    setShow(!show);
+  };
+  return (
+    <div>
+      <p>
+        {item.title}
+        {show ? (
+          <AiOutlineMinus onClick={flip} />
+        ) : (
+          <AiOutlinePlus onClick={flip} />
+        )}
+      </p>
+      <p>{show && item.info}</p>
+    </div>
+  );
 };
 
 export default Question;
