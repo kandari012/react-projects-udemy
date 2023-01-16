b3dba46a77d7b425ce61215576d749db1800e217
rahul kandari <rahulkandari012@gmail.com>
2023-01-16 12:00:34 +0530

    context file,onmouse show text,get info of button from dom,inlinr css

--- 13-stripe-submenus/setup/src/App.js
+++ 13-stripe-submenus/setup/src/App.js
@@ -1,14 +1,19 @@
-import React from 'react'
-import Navbar from './Navbar'
-import Hero from './Hero'
-import Sidebar from './Sidebar'
-import Submenu from './Submenu'
+import React from "react";
+import Navbar from "./Navbar";
+import Hero from "./Hero";
+import Sidebar from "./Sidebar";
+import Submenu from "./Submenu";
+import { useGlobalContext } from "./context";
+
 function App() {
+  const c = useGlobalContext();
+  console.log(c);
   return (
     <>
       <h2>stripe submenus setup</h2>
+      <h2>{c}</h2>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- 13-stripe-submenus/setup/src/context.js
+++ 13-stripe-submenus/setup/src/context.js
@@ -1,2 +1,16 @@
-import React, { useState, useContext } from 'react'
-import sublinks from './data'
+import React, { useState, useContext } from "react";
+import sublinks from "./data";
+
+const AppContext = React.createContext();
+
+export const AppProvider = ({ children }) => {
+  return (
+    <AppContext.Provider value={{ x: "abc", y: "xyz" }}>
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
