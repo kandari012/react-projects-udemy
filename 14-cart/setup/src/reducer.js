014d7accda430a27cd7cc3d7c2f84f4f8464747e
rahul kandari <rahulkandari012@gmail.com>
2023-01-18 13:59:07 +0530

    14-cart project icrease,decrease,clear cart

--- 14-cart/setup/src/reducer.js
+++ 14-cart/setup/src/reducer.js
@@ -2,6 +2,45 @@ const reducer = (state, action) => {
   if (action.type === "CLEAR_CART") {
     return { ...state, cart: [] };
   }
+  if (action.type === "REMOVE") {
+    let cart = state.cart.filter((item) => item.id !== action.payload);
+    return {
+      ...state,
+      cart: cart,
+    };
+  }
+
+  if (action.type === "INCREASE") {
+    let tempCart = state.cart.map((item) => {
+      if (item.id === action.payload) {
+        return { ...item, amount: item.amount + 1 };
+      } else {
+        return item;
+      }
+    });
+
+    return {
+      ...state,
+      cart: tempCart,
+    };
+  }
+
+  if (action.type === "DECREASE") {
+    let tempCart = state.cart
+      .map((item) => {
+        if (item.id === action.payload) {
+          return { ...item, amount: item.amount - 1 };
+        } else {
+          return item;
+        }
+      })
+      .filter((item) => item.amount !== 0);
+
+    return {
+      ...state,
+      cart: tempCart,
+    };
+  }
   return state;
 };
 
