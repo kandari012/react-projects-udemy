929f4786697b28490ee3d712b39a866101cdcf4d
rahul kandari <rahulkandari012@gmail.com>
2023-02-06 12:27:14 +0530

    15 cocktail router setup

--- 15-cocktails/setup/src/components/Navbar.js
+++ 15-cocktails/setup/src/components/Navbar.js
@@ -1,13 +1,24 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import logo from '../logo.svg'
-
+import { Link } from "react-router-dom";
+import logo from "../logo.svg";
 const Navbar = () => {
   return (
-    <div>
-      <h2>navbar component</h2>
-    </div>
-  )
-}
-
-export default Navbar
+    <nav className="navbar">
+      <Link to="/">
+        <img src={logo} alt="cocktail logo"></img>
+      </Link>
+      <ul>
+        <li>
+          {" "}
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/cocktail/:id">Products</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+export default Navbar;
